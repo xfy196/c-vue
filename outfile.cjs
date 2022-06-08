250db043b01e13ba40c38052b742fbe9a5f30723
许富荧 <xufuying@xufuyingdeMacBook-Pro.local>
2022-06-08 09:39:45 +0800

    优化keywords关键字

--- outfile.cjs
+++ outfile.cjs
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-/*! quick-vue-cli v1.1.0 | MIT */
+/*! quick-vue-cli v1.1.1 | MIT */
 var __defProp = Object.defineProperty;
 var __defProps = Object.defineProperties;
 var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
